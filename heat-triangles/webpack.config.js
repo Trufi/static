fe6ba620e3950d1e1bde30faf983605a24f91f43
Mstislav Zhivodkov <stevemyz@gmail.com>
2020-02-04 14:58:20 +0700

    Add heat triangles

---
+++ heat-triangles/webpack.config.js
@@ -0,0 +1,55 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+
+module.exports = (_, args) => {
+    const mode = args.production ? 'production' : 'development';
+
+    return {
+        mode,
+
+        module: {
+            rules: [
+                {
+                    test: /(\.ts|\.tsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                },
+            ],
+        },
+
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+
+        entry: './src/index.ts',
+
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/dist',
+        },
+
+        plugins: [
+            new ForkTsCheckerWebpackPlugin({
+                watch: ['./src'],
+            }),
+        ],
+
+        devtool: mode === 'production' ? false : 'source-map',
+
+        devServer: {
+            host: '0.0.0.0',
+            port: 3000,
+            stats: {
+                modules: false,
+            },
+            contentBase: path.resolve(__dirname, 'dist'),
+            disableHostCheck: true,
+        },
+    };
+};
